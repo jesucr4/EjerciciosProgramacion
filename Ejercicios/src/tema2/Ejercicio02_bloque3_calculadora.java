f03d71dfcde880840f010281d537511cef4c68af
EQUIPO <EQUIPO@192.168.1.38>
2019-10-02 19:31:20 +0200

    Casa

--- Ejercicios/src/tema2/Ejercicio02_bloque3_calculadora.java
+++ Ejercicios/src/tema2/Ejercicio02_bloque3_calculadora.java
@@ -12,6 +12,28 @@ public class Ejercicio02_bloque3_calculadora {
 			case 1: 
 				str = JOptionPane.showInputDialog("Introduzca nÃºmero para raÃ­z cuadrada");
 				float num1 = Float.parseFloat(str);
+				float num2 = ((float) Math.sqrt(num1));
+				System.out.println("La raíz cuadrada del número introducid es " + num2);
+					break;
+			case 2:
+				str = JOptionPane.showInputDialog("Introduzca número base para potencia");
+				float numBase = Float.parseFloat(str);
+				str = JOptionPane.showInputDialog("Introduzca número para exponente potencia");
+				float numExponente = Float.parseFloat(str);
+				float potencia = (float) Math.pow(numBase, numExponente);
+				System.out.println("El resultado de la potencia introducida es " + potencia);
+					break;
+			case 3:
+				str = JOptionPane.showInputDialog("Introduzca número para dividendo");
+				float dividendo = Float.parseFloat(str);
+				str = JOptionPane.showInputDialog("Introduzca número para divisor");
+				int divisor = Integer.parseInt(str);
+				int division = (int) (dividendo % divisor);
+				System.out.println("El resto de la división introducida es " + division);
+					break;
+				default:
+					System.out.println("El número introducido no conlleva ninguna operación");
+					
 				
 		}
 	}
