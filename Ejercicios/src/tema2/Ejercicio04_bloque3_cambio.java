610184c6d2e952bdb16fd107bc4065d743986a2e
diurno <diurno@pc-aula>
2019-10-04 13:00:38 +0200

    clase 04/10

---
+++ Ejercicios/src/tema2/Ejercicio04_bloque3_cambio.java
@@ -0,0 +1,15 @@
+package tema2;
+
+public class Ejercicio04_bloque3_cambio {
+
+	public static void main(String[] args) {
+		int precio = 6743;
+		int pago = 10000;
+		
+		int cambioADevolver = pago - precio;
+		
+		//Cálculo la cantidad de billetes de 100,00
+
+	}
+
+}
---
+++ Ejercicios/src/tema3_bucles/Ejercicio01_sumaMayor10.java
@@ -0,0 +1,20 @@
+package tema3_bucles;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio01_sumaMayor10 {
+
+	public static void main(String[] args) {
+		// pedimos números al usuario
+		int primerNumero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca primer número"));
+		int segundoNumero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca segundo número"));
+		int tercerNumero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca tercer número"));
+		int cuartoNumero = Integer.parseInt(JOptionPane.showInputDialog("Introduzca cuarto número"));
+		
+		//creamos variables para bucle for
+		int minimo = 10;
+		
+		for ( i = primerNumero; primerNumero)
+	}
+
+}
