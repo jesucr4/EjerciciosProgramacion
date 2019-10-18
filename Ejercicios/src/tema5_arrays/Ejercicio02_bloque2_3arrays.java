59139cd1decadbe4d5739a3f3055fb5ea1c97a04
diurno <diurno@pc-aula>
2019-10-18 14:49:36 +0200

    18/10

--- Ejercicios/src/tema5_arrays/Ejercicio02_bloque2_3arrays.java
+++ Ejercicios/src/tema5_arrays/Ejercicio02_bloque2_3arrays.java
@@ -3,6 +3,8 @@ package tema5_arrays;
 public class Ejercicio02_bloque2_3arrays {
 
 	public static void main(String[] args) {
+		//creamos variables número par y número impar
+		
 		//creamos primer array y mostramos en pantalla
 		int primerArray[]= Utils.creaArrayNumerosAzar(5, 0, 10);
 		Utils.mostrarArray(primerArray);
@@ -18,24 +20,29 @@ public class Ejercicio02_bloque2_3arrays {
 		
 		for (int i = 0; i < primerArray.length; i++) {
 			if (i%2 == 1) {
+				int impar = i;
 				for (int j = 0; j < tercerArray.length; j++) {
 					if (j%2 == 1) {
-						j = i;
+						tercerArray[j]=impar;
 					}
 				}
 				
 			}
 		}
-		for (int i = 0; i < segundoArray.length; i++) {
+		
+		for ( int i = 0; i < segundoArray.length; i++) {
 			if (i%2 == 0) {
+				int par = i;
 				for (int j = 0; j < tercerArray.length; j++) {
 					if (j%2 == 0) {
-						j=i;
+					 tercerArray[j] = par;
 					}
 				}
 			}
 		}
 		Utils.mostrarArray(tercerArray);
-	}
-
+		}
+		
 }
+
+
