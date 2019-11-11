9b02f65bb0f43c8daa806e266e85fca9ad10b3c3
diurno <diurno@pc-aula>
2019-11-11 14:57:39 +0100

    subida clase 11/11

---
+++ Ejercicios/src/tema4_OrientaciónaObjetos_Bloque3_Ejercicio03/Persona.java
@@ -0,0 +1,26 @@
+package tema4_OrientaciónaObjetos_Bloque3_Ejercicio03;
+
+public class Persona {
+	String Nombre;
+	String Apellidos;
+	String DNI;
+	String dirección;
+	int teléfono;
+
+
+	public Persona (String NuevoNombre, String NuevoApellido, String NuevoDNI, String NuevaDirección, int NuevoTlfn) {
+		Nombre = NuevoNombre;
+		Apellidos = NuevoApellido;
+		DNI = NuevoDNI;
+		dirección = NuevaDirección;
+		teléfono = NuevoTlfn;
+	}
+	
+	public void DatosPersona () {
+		
+	}
+
+}
+
+
+
---
+++ Ejercicios/src/tema4_OrientaciónaObjetos_Bloque3_Ejercicio03/PruebaPersona.java
@@ -0,0 +1,15 @@
+package tema4_OrientaciónaObjetos_Bloque3_Ejercicio03;
+
+import javax.swing.JOptionPane;
+
+public class PruebaPersona {
+
+	public static void main(String[] args) {
+		int opcion = Integer.parseInt(JOptionPane.showInputDialog("¿Quiere crear nuevo usuario? Pulse: \n -0:Sí \n -1:No"));
+		if (opcion == 1) {
+			Persona persona1 = new Persona;
+		}
+		}
+}
+
+
