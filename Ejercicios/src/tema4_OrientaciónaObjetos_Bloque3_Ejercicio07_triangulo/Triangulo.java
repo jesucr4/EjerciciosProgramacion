86ffbe4b4e869e5e7863ae996f48d2f297f9b1fa
diurno <diurno@pc-aula>
2019-11-25 14:56:32 +0100

    clase 25/11

---
+++ Ejercicios/src/tema4_OrientaciónaObjetos_Bloque3_Ejercicio07_triangulo/Triangulo.java
@@ -0,0 +1,32 @@
+package tema4_OrientaciónaObjetos_Bloque3_Ejercicio07_triangulo;
+
+public class Triangulo {
+		private int alto;
+		private int ancho;
+		/**
+		 * 
+		 * @param alto
+		 * @param ancho
+		 */
+		public Triangulo(int alto, int ancho) {
+			super();
+			this.alto = alto;
+			this.ancho = ancho;
+		}
+		
+
+		
+		
+		public void imprimir ()	{
+			int contadorAsteriscosPorFila = 1;
+			for (int i = 0; i < this.alto; i++) {	
+				for (int j = 0; j < contadorAsteriscosPorFila; j++) {
+					System.out.print("*");
+				}
+				contadorAsteriscosPorFila++;
+				System.out.println();
+			}
+		}
+		
+		
+}
